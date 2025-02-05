a6bcb1c2f58aaeabe3bc83c66e388a77df94e13e
unknown <Achaqilal407@gmail.com>
2025-02-05 22:15:56 +0800

    done auth

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,25 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import router from "./routes/route.js";
+import path from "path";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use('/api', router);
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, Express + TypeScript!");
-});
+app.use(cookieParser());
 
 const PORT = process.env.PORT || 5005;
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV !== "development") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 app.listen(PORT, () => {
   console.log(`Server berjalan di http://localhost:${PORT}`);
 });
--- src/routes/route.ts
+++ src/routes/route.ts
@@ -1,7 +1,13 @@
 import express from "express";
+import { getUser, login, logout, register, updateUser } from "../controller/auth-controller.js";
+import protectRoute from "../middleware/auth-middleware.js";
 
 const router = express.Router();
 
-
+router.post("/register", register);
+router.post("/login", login);
+router.get("get-user", protectRoute, getUser);
+router.patch("/update-user", protectRoute, updateUser);
+router.delete("/logout", protectRoute, logout);
 
 export default router;
