aec68bd3b544170b5829a8dd80a8eaa58debf98d
unknown <Achaqilal407@gmail.com>
2025-02-11 17:52:34 +0800

    done message

--- src/controller/message-controller.ts
+++ src/controller/message-controller.ts
@@ -9,7 +9,36 @@ export const sendMessage = async (req: Request, res: Response) => {
 		if (!req.user) {
             return res.status(401).json({ error: "Unauthorized - User not found" });
         }
+
+		if(!message) {
+			return res.status(400).json({
+				code: 400,
+				status: "error",
+				message: "pesan tidak boleh kosong"
+			});
+		}
+
+		if(!receiverId) {
+			return res.status(400).json({
+				code: 400,
+				status: "error",
+				message: "id penerima tidak valid"
+			})
+		}
+
         const senderId = req.user.id;
+
+		const receiverExists = await prisma.user.findUnique({
+			where: { id: receiverId },
+		});
+
+		if (!receiverExists) {
+			return res.status(404).json({
+                code: 404,
+                status: "error",
+                message: "User penerima tidak ditemukan"
+            });
+		}
 
 		let conversation = await prisma.conversation.findFirst({
 			where: {
@@ -58,7 +87,12 @@ export const sendMessage = async (req: Request, res: Response) => {
 			io.to(receiverSocketId).emit("newMessage", newMessage);
 		}
 
-		res.status(201).json(newMessage);
+		res.status(201).json({
+			code : 201,
+			status : "succes",
+			message : "pesan berhasil dikirim",
+			data : newMessage
+		});
 	} catch (error: any) {
 		console.error("Error in sendMessage: ", error.message);
 		res.status(500).json({ error: "Internal server error" });
@@ -68,10 +102,31 @@ export const sendMessage = async (req: Request, res: Response) => {
 export const getMessages = async (req: Request, res: Response) => {
 	try {
 		const { id: userToChatId } = req.params;
+
+		if(!userToChatId) {
+			return res.status(400).json({
+				code: 400,
+				status: "error",
+				message: "id penerima tidak valid"
+			})
+		}
+
 		if (!req.user) {
             return res.status(401).json({ error: "Unauthorized - User not found" });
         }
         const senderId = req.user.id;
+
+		const userExists = await prisma.user.findUnique({
+			where: { id: userToChatId },
+		});
+
+		if(!userExists) {
+			return res.status(404).json({
+				code: 404,
+				status: "error",
+				message: "User penerima tidak ditemukan"
+			});
+		}
 
 		const conversation = await prisma.conversation.findFirst({
 			where: {
@@ -89,10 +144,20 @@ export const getMessages = async (req: Request, res: Response) => {
 		});
 
 		if (!conversation) {
-			return res.status(200).json([]);
+			return res.status(200).json({
+				code: 200,
+				status: "success",
+				message: "belum ada percakapan",
+				data: []
+			});
 		}
 
-		res.status(200).json(conversation.messages);
+		res.status(200).json({
+			code: 200,
+			status: "success",
+			message: "berhasil mendapatkan pesan",
+			data: conversation.messages
+		})
 	} catch (error: any) {
 		console.error("Error in getMessages: ", error.message);
 		res.status(500).json({ error: "Internal server error" });
@@ -120,7 +185,12 @@ export const getUsersForSidebar = async (req: Request, res: Response) => {
 			},
 		});
 
-		res.status(200).json(users);
+		res.status(200).json({
+			code: 200,
+			status: "success",
+			message: "berhasil mendapatkan data user",
+			data: users
+		})
 	} catch (error: any) {
 		console.error("Error in getUsersForSidebar: ", error.message);
 		res.status(500).json({ error: "Internal server error" });
